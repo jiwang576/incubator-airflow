226aa8ad5aa848d99a5d30a5cdbd4e1a568c7311
Ji Wang <jiwangmtv@google.com>
2017-06-20 20:16:16 +0000

    [AIRFLOW-1272] Standardize to single quotes

--- airflow/contrib/operators/cloudml_operator.py
+++ airflow/contrib/operators/cloudml_operator.py
@@ -24,7 +24,7 @@ from airflow.contrib.hooks.gcp_cloudml_hook import CloudMLHook
 from airflow.utils.decorators import apply_defaults
 from apiclient import errors
 
-logging.getLogger("GoogleCloudML").setLevel(logging.INFO)
+logging.getLogger('GoogleCloudML').setLevel(logging.INFO)
 
 
 def _normalize_cloudml_job_id(job_id):
@@ -39,10 +39,10 @@ def _normalize_cloudml_job_id(job_id):
     Returns:
         A valid job_id representation.
     """
-    match = re.search(r"\d", job_id)
+    match = re.search(r'\d', job_id)
     if match and match.start() is 0:
-        job_id = "z_{}".format(job_id)
-    return re.sub("[^0-9a-zA-Z]+", "_", job_id)
+        job_id = 'z_{}'.format(job_id)
+    return re.sub('[^0-9a-zA-Z]+', '_', job_id)
 
 
 def _create_prediction_input(project_id,
@@ -70,69 +70,69 @@ def _create_prediction_input(project_id,
         from the inputs.
     """
 
-    if data_format not in ["TEXT", "TF_RECORD", "TF_RECORD_GZIP"]:
+    if data_format not in ['TEXT', 'TF_RECORD', 'TF_RECORD_GZIP']:
         logging.warning(
-            "The input data format is not recognized. Default to "
-            "DATA_FORMAT_UNSPECIFIED.")
-        data_format = "DATA_FORMAT_UNSPECIFIED"
+            'The input data format is not recognized. Default to '
+            'DATA_FORMAT_UNSPECIFIED.')
+        data_format = 'DATA_FORMAT_UNSPECIFIED'
 
     try:
         for input_path in input_paths:
             _validate_gcs_uri(input_path)
     except ValueError as e:
-        raise ValueError("Illegal input path. " + str(e))
+        raise ValueError('Illegal input path. ' + str(e))
 
     try:
         _validate_gcs_uri(output_path)
     except ValueError as e:
-        raise ValueError("Illegal output path. " + str(e))
+        raise ValueError('Illegal output path. ' + str(e))
 
     prediction_input = {
-        "dataFormat": data_format,
-        "inputPaths": input_paths,
-        "outputPath": output_path,
-        "region": region
+        'dataFormat': data_format,
+        'inputPaths': input_paths,
+        'outputPath': output_path,
+        'region': region
     }
 
     if uri:
         if model_name or version_name:
             logging.error(
-                "Ambiguous model origin: Both uri and model/version name are "
-                "provided.")
-            raise ValueError("Ambiguous model origin.")
+                'Ambiguous model origin: Both uri and model/version name are '
+                'provided.')
+            raise ValueError('Ambiguous model origin.')
         try:
             _validate_gcs_uri(uri)
-            prediction_input["uri"] = uri
+            prediction_input['uri'] = uri
         except ValueError as e:
-            logging.error("Illegal uri: " + uri)
+            logging.error('Illegal uri: ' + uri)
             raise e
 
     elif version_name:
         if not model_name:
             logging.error(
-                "Incomplete model origin: A version name is provided but the "
-                "corresponding model name is missing.")
-            raise ValueError("Incomplete version origin.")
+                'Incomplete model origin: A version name is provided but the '
+                'corresponding model name is missing.')
+            raise ValueError('Incomplete version origin.')
         try:
-            prediction_input["versionName"] = _create_origin_name(
+            prediction_input['versionName'] = _create_origin_name(
                 project_id, model_name, version_name)
         except ValueError as e:
-            logging.error("Error constructing the model version origin.")
+            logging.error('Error constructing the model version origin.')
             raise e
 
     elif model_name:
         try:
-            prediction_input["modelName"] = _create_origin_name(
+            prediction_input['modelName'] = _create_origin_name(
                 project_id, model_name)
         except ValueError as e:
-            logging.error("Error constructing the model origin.")
+            logging.error('Error constructing the model origin.')
             raise e
 
     else:
         logging.error(
-            "Missing model origin: Batch prediction expects a model, "
-            "a version, or a URI to savedModel.")
-        raise ValueError("Missing model version origin.")
+            'Missing model origin: Batch prediction expects a model, '
+            'a version, or a URI to savedModel.')
+        raise ValueError('Missing model version origin.')
 
     # Check for non-integer string or non-positive input
     if max_worker_count:
@@ -140,13 +140,13 @@ def _create_prediction_input(project_id,
             count = int(max_worker_count)
             if count < 0:
                 raise ValueError(
-                    "The maximum worker count cannot be non-positive.")
-            prediction_input["maxWorkerCount"] = max_worker_count
+                    'The maximum worker count cannot be non-positive.')
+            prediction_input['maxWorkerCount'] = max_worker_count
         except ValueError as e:
             raise e
 
     if runtime_version:
-        prediction_input["runtimeVersion"] = runtime_version
+        prediction_input['runtimeVersion'] = runtime_version
 
     return prediction_input
 
@@ -164,54 +164,54 @@ def _validate_gcs_uri(uri):
     Raises:
         ValueError: if the URI is invalid.
     """
-    if not re.match(r"^gs://", uri):
-        raise ValueError("Missing GCS scheme in the URI: {}.".format(uri))
+    if not re.match(r'^gs://', uri):
+        raise ValueError('Missing GCS scheme in the URI: {}.'.format(uri))
 
     try:
-        bucket_name, object_name = uri[len(r"gs://"):].split("/", 1)
+        bucket_name, object_name = uri[len(r'gs://'):].split('/', 1)
     except ValueError:
-        raise ValueError("Illegal URI for a GCS object: {}.".format(uri))
+        raise ValueError('Illegal URI for a GCS object: {}.'.format(uri))
 
     # Validation for bucket name
-    if not re.match(r"^[a-zA-Z0-9][-.\w]*[a-zA-Z0-9]$", bucket_name):
-        raise ValueError("Illegal GCS bucket name: {}.".format(bucket_name))
-    if "." in bucket_name and len(bucket_name) > 222:
+    if not re.match(r'^[a-zA-Z0-9][-.\w]*[a-zA-Z0-9]$', bucket_name):
+        raise ValueError('Illegal GCS bucket name: {}.'.format(bucket_name))
+    if '.' in bucket_name and len(bucket_name) > 222:
         raise ValueError(
-            "GCS bucket name is longer than 222 bytes: {}."
+            'GCS bucket name is longer than 222 bytes: {}.'
             .format(bucket_name))
-    for dot_separated_component in bucket_name.split("."):
+    for dot_separated_component in bucket_name.split('.'):
         if not 3 <= len(dot_separated_component) <= 63:
             raise ValueError(
-                "Each dot-eseparated component in GCS bucket name should be "
-                "within 3 and 63 characters: {}.".format(bucket_name))
-    if re.match(r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b", bucket_name):
+                'Each dot-eseparated component in GCS bucket name should be '
+                'within 3 and 63 characters: {}.'.format(bucket_name))
+    if re.match(r'\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b', bucket_name):
         raise ValueError(
-            "GCS bucket name must not represent a IPv4-like sequence: {}."
+            'GCS bucket name must not represent a IPv4-like sequence: {}.'
             .format(bucket_name))
-    if re.match(r"(google|^goog)", bucket_name):
+    if re.match(r'(google|^goog)', bucket_name):
         raise ValueError(
-            "GCS bucket name should not start with 'goog' prefix, or contain "
-            "'google' inside the name: {}.".format(bucket_name))
-    if re.match(r"(\.[-.])|(-\.)", bucket_name):
+            'GCS bucket name should not start with "goog" prefix, or contain '
+            '"google" inside the name: {}.'.format(bucket_name))
+    if re.match(r'(\.[-.])|(-\.)', bucket_name):
         raise ValueError(
-            "Bucket names must not contain a dot next to either a dot or a "
-            "dash: {}.".format(bucket_name))
+            'Bucket names must not contain a dot next to either a dot or a '
+            'dash: {}.'.format(bucket_name))
 
     # Validation for object name
     try:
-        object_name_utf8 = object_name.decode("utf-8")
+        object_name_utf8 = object_name.decode('utf-8')
     except AttributeError:
         object_name_utf8 = object_name
-        
+
     if not 1 <= len(object_name_utf8) <= 1024:
         raise ValueError(
-            "GCS object name should be within 1 and 1024 bytes in length: {}.".
+            'GCS object name should be within 1 and 1024 bytes in length: {}.'.
             format(uri))
-    if r"\r" in object_name_utf8 or r"\n" in object_name_utf8:
+    if r'\r' in object_name_utf8 or r'\n' in object_name_utf8:
         raise ValueError(
-            "GCS object name must not contain {} characters: {}."
+            'GCS object name must not contain {} characters: {}.'
             .format(
-                "Carriage Return" if r"\r" in object_name_utf8 else "Line Feed",
+                'Carriage Return' if r'\r' in object_name_utf8 else 'Line Feed',
                 uri))
 
 
@@ -234,22 +234,22 @@ def _create_origin_name(project_id, model_name, version_name=None):
     """
     try:
         _validate_project_id(project_id)
-        origin_name = "projects/{}".format(project_id)
+        origin_name = 'projects/{}'.format(project_id)
     except ValueError as e:
-        raise ValueError("Error parsing the project id. " + str(e))
+        raise ValueError('Error parsing the project id. ' + str(e))
 
     try:
         _validate_resource_name(model_name)
-        origin_name += "/models/{}".format(model_name)
+        origin_name += '/models/{}'.format(model_name)
     except ValueError as e:
-        raise ValueError("Error parsing the model name. " + str(e))
+        raise ValueError('Error parsing the model name. ' + str(e))
 
     if version_name:
         try:
             _validate_resource_name(version_name)
-            origin_name += "/versions/{}".format(version_name)
+            origin_name += '/versions/{}'.format(version_name)
         except ValueError as e:
-            raise ValueError("Error parsing the version name. " + str(e))
+            raise ValueError('Error parsing the version name. ' + str(e))
 
     return origin_name
 
@@ -269,10 +269,10 @@ def _validate_project_id(project_id):
     """
     if not 6 <= len(project_id) <= 30:
         raise ValueError(
-            "Project id should be within 6 and 30 characters in length: {}."
+            'Project id should be within 6 and 30 characters in length: {}.'
             .format(project_id))
-    if not re.match(r"^[a-z][-a-z0-9]*[^-]$", project_id):
-        raise ValueError("Illegal project id: {}.".format(project_id))
+    if not re.match(r'^[a-z][-a-z0-9]*[^-]$', project_id):
+        raise ValueError('Illegal project id: {}.'.format(project_id))
 
 
 def _validate_resource_name(resource_name):
@@ -291,10 +291,10 @@ def _validate_resource_name(resource_name):
     """
     if not 1 <= len(resource_name) <= 128:
         raise ValueError(
-            "Resource name should be within 1 and 128 characters in length: {}."
+            'Resource name should be within 1 and 128 characters in length: {}.'
             .format(resource_name))
-    if not re.match(r"^[a-zA-Z]\w*$", resource_name):
-        raise ValueError("The resource name is illegal: {}.".format(
+    if not re.match(r'^[a-zA-Z]\w*$', resource_name):
+        raise ValueError('The resource name is illegal: {}.'.format(
             resource_name))
 
 
@@ -381,8 +381,8 @@ class CloudMLBatchPredictionOperator(BaseOperator):
     """
 
     template_fields = [
-        "project_id", "job_id", "input_paths", "output_path", "model_name",
-        "version_name", "uri"
+        'project_id', 'job_id', 'input_paths', 'output_path', 'model_name',
+        'version_name', 'uri'
     ]
 
     @apply_defaults
@@ -398,7 +398,7 @@ class CloudMLBatchPredictionOperator(BaseOperator):
                  uri=None,
                  max_worker_count=None,
                  runtime_version=None,
-                 gcp_conn_id="google_cloud_default",
+                 gcp_conn_id='google_cloud_default',
                  delegate_to=None,
                  *args,
                  **kwargs):
@@ -425,11 +425,11 @@ class CloudMLBatchPredictionOperator(BaseOperator):
             raise e
 
         hook = CloudMLHook(self.gcp_conn_id, self.delegate_to)
-        job_id = _normalize_cloudml_job_id(prediction_job_request["jobId"])
+        job_id = _normalize_cloudml_job_id(prediction_job_request['jobId'])
         try:
             existing_job = hook.get_job(self.project_id, job_id)
             logging.info(
-                "Job with job_id {} already exist: {}.".format(
+                'Job with job_id {} already exist: {}.'.format(
                     job_id,
                     existing_job))
             finished_prediction_job = hook.wait_for_job_done(
@@ -438,7 +438,7 @@ class CloudMLBatchPredictionOperator(BaseOperator):
         except errors.HttpError as e:
             if e.resp.status == 404:
                 logging.error(
-                    "Job with job_id {} does not exist. Will create it."
+                    'Job with job_id {} does not exist. Will create it.'
                     .format(job_id))
                 finished_prediction_job = hook.create_job(
                     self.project_id,
@@ -446,22 +446,22 @@ class CloudMLBatchPredictionOperator(BaseOperator):
             else:
                 raise e
 
-        if finished_prediction_job["state"] != "SUCCEEDED":
+        if finished_prediction_job['state'] != 'SUCCEEDED':
             logging.error(
-                "Batch prediction job failed: %s",
+                'Batch prediction job failed: %s',
                 str(finished_prediction_job))
-            raise RuntimeError(finished_prediction_job["errorMessage"])
+            raise RuntimeError(finished_prediction_job['errorMessage'])
 
-        return finished_prediction_job["predictionOutput"]
+        return finished_prediction_job['predictionOutput']
 
     def _create_valid_job_request(self):
         try:
             _validate_resource_name(self.job_id)
         except ValueError as e:
             logging.error(
-                "Cannot create batch prediction job request due to: {}"
+                'Cannot create batch prediction job request due to: {}'
                 .format(str(e)))
-            raise ValueError("Illegal job id. " + str(e))
+            raise ValueError('Illegal job id. ' + str(e))
 
         try:
             prediction_input = _create_prediction_input(
@@ -471,10 +471,10 @@ class CloudMLBatchPredictionOperator(BaseOperator):
                 self.runtime_version)
         except ValueError as e:
             logging.error(
-                "Cannot create batch prediction job request due to: {}"
+                'Cannot create batch prediction job request due to: {}'
                 .format(str(e)))
             raise e
 
         return {
-            "jobId": self.job_id,
-            "predictionInput": prediction_input}
+            'jobId': self.job_id,
+            'predictionInput': prediction_input}
